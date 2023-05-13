fba73591a7a959f1d09e187c8eca874c71dc8372
ezluci <ezluci1@gmail.com>
2023-05-13 11:28:49 +0300

    Added some FPS calculations and movement.

--- src/js/game.js
+++ src/js/game.js
@@ -7,6 +7,7 @@ const OFFSET_DOWN = 10
 const BLOCKS_HORIZONTALLY = 15
 const BLOCKS_VERTICALLY = 11
 const BLOCK_SIZE = 53
+const MOVE_SPEED = 0.1
 
 let imagesLoaded = 0
 let whitePlrImg, blackPlrImg
@@ -36,13 +37,6 @@ loadImage('assets/block.jpg').then(image => {
    imagesLoaded ++
 })
 
-const intervalID = setInterval(() => {
-   if (imagesLoaded === 3) {
-      clearInterval(intervalID)
-      later()
-   }
-}, 30)
-
 
 // this function draws a block using block coordinates => x=0..15 and y=0.11
 function drawBlock(block, xBlock, yBlock) {
@@ -51,7 +45,7 @@ function drawBlock(block, xBlock, yBlock) {
 
 // this function draws a player using normal coordinates WITHOUT OFFSET
 function drawPlayer(player, x, y) {
-   ctx.drawImage(player, OFFSET_LEFT + x, OFFSET_UP + y)
+   ctx.drawImage(player, OFFSET_LEFT + x, OFFSET_UP + y, BLOCK_SIZE, BLOCK_SIZE)
 }
 
 
@@ -78,19 +72,59 @@ document.onkeyup = (event) => {
 }
 
 
-function later() {
-   document.querySelector('#loading').hidden = true
-   
+const fpsElem = document.querySelector('#fps')
+const me = {x: 0, y: 0}
+let lastFrameTime
+
+const intervalID = setInterval(() => {
+   if (imagesLoaded === 3) {
+      clearInterval(intervalID)
+
+      document.querySelector('#loading').hidden = true
+      lastFrameTime = performance.now()
+      window.requestAnimationFrame(gameloop)
+   }
+}, 25)
+
+
+function gameloop() {
+   // calculate deltaTime and FPS
+   const currentTime = performance.now()
+   const deltaTime = currentTime - lastFrameTime
+   lastFrameTime = currentTime
+   const FPS = 1000 / deltaTime
+
+
+   /// UPDATES
+
+   if (keys['a'])
+      me.x -= MOVE_SPEED * deltaTime
+   if (keys['d'])
+      me.x += MOVE_SPEED * deltaTime
+   if (keys['s'])
+      me.y += MOVE_SPEED * deltaTime
+   if (keys['w'])
+      me.y -= MOVE_SPEED * deltaTime
+
+
+   /// DRAWING
+
    ctx.fillStyle = '#0C0C06'
-   ctx.fillRect(0, 0, canvas.width, canvas.clientHeight)
+   ctx.fillRect(0, 0, canvas.width, canvas.height)
 
    ctx.fillStyle = 'rgba(131, 29, 29, 0.541)'
    ctx.fillRect(OFFSET_LEFT, OFFSET_UP, canvas.width - OFFSET_LEFT - OFFSET_RIGHT, canvas.height - OFFSET_UP - OFFSET_DOWN)
 
-   drawBlock(whitePlrImg, 0, 0)
-   drawBlock(blackPlrImg, BLOCKS_HORIZONTALLY - 1, BLOCKS_VERTICALLY - 1)
+   drawPlayer(whitePlrImg, me.x, me.y)
+   drawPlayer(blackPlrImg, (BLOCKS_HORIZONTALLY - 1) * BLOCK_SIZE, (BLOCKS_VERTICALLY - 1) * BLOCK_SIZE)
 
    for (let yb = 1; yb < BLOCKS_VERTICALLY; yb += 2)
       for (let xb = 1; xb < BLOCKS_HORIZONTALLY; xb += 2)
          drawBlock(blockImg, xb, yb)
+   
+   ctx.fillStyle = 'gray'
+   ctx.font = '30px serif'
+   ctx.fillText(`FPS: ${Math.floor(FPS)}, deltaTime: ${Math.floor(deltaTime)}`, 20, 23)
+   
+   window.requestAnimationFrame(gameloop)
 }
