a5f1c104eb6152ee9c7fc03dc8a99c5f5532f6e0
ezluci <ezluci1@gmail.com>
2023-05-13 22:19:37 +0300

    Fixed colors between players. Added some checks to prevent random connects to websocket.

--- src/server.js
+++ src/server.js
@@ -14,6 +14,12 @@ server.listen(parseInt(PORT), () => {
    console.log(`Server-ul functioneaza pe portul ${PORT}.`)
 })
 
+const ROOM_STATUS = {
+   WAITING: 'waiting',
+   STARTING: 'starting',
+   STARTED: 'started'
+}
+
 const IDS = new Map() // info about all socket_ids
 const ROOMS = new Map() // info about all rooms
 
@@ -21,14 +27,23 @@ io.on('connection', (socket) => {
 
    let username, room, color, isOwner
 
+   function detailsOkCheck() {
+      if (!username) {
+         socket.emit('error', 'Server does not have socket details (username, room, etc.). Probably playerJoined was never emitted. DISCONNECTED.')
+         socket.disconnect()
+         return false
+      }
+      return true
+   }
+
    socket.on('playerJoined', (username1, room1, callback) => {
       if (!username1) {
-         socket.emit('error', 'invalid username')
+         socket.emit('error', 'playerJoined: invalid username. DISCONNECTED.')
          socket.disconnect()
          return
       }
       if (!room1) {
-         socket.emit('error', 'invalid room')
+         socket.emit('error', 'playerJoined: invalid room. DISCONNECTED.')
          socket.disconnect()
          return
       }
@@ -38,7 +53,9 @@ io.on('connection', (socket) => {
       color = 'spectator'
       if (!io.sockets.adapter.rooms.get(room)) {
          isOwner = true
-         ROOMS.set(room, {owner: username, whiteTaken: false, blackTaken: false, orangeTaken: false, greenTaken: false})
+         ROOMS.set(room, {owner: username, whiteTaken: false, blackTaken: false, orangeTaken: false,
+            greenTaken: false, status: ROOM_STATUS.WAITING}
+         )
       }
       isOwner = !(io.sockets.adapter.rooms.get(room))
 
@@ -61,26 +78,42 @@ io.on('connection', (socket) => {
 
 
    socket.on('tryStart', () => {
+      if (!detailsOkCheck())
+         return
+      
+      if (ROOMS.get(room).status !== ROOM_STATUS.WAITING)
+         return socket.emit('error', 'tryStart: Cannot start game. Room is not in WAITING status.')
+      
       io.to(room).emit('begin_startCountdown', username)
    })
 
 
-   socket.on('selectColor', (color1) => {
-      if (color1 !== 'spectator' || color1 !== 'white' || color1 !== 'black' || color1 !== 'orange' || color1 !== 'green')
-         return socket.emit('error', 'invalid color for selectColor.')
+   socket.on('selectColor', (newColor, callback) => {
+      if (!detailsOkCheck())
+         return
       
-      if (color1 !== 'spectator' && ROOMS.get(room)[`${color1}Taken`])
-         return socket.emit('error', 'color already taken')
+      if (newColor !== 'spectator' && newColor !== 'white' && newColor !== 'black' && newColor !== 'orange' && newColor !== 'green')
+         return socket.emit('error', 'selectColor: invalid color.')
       
-      ROOMS.get(room)[`${color}Taken`] = false
-      ROOMS.get(room)[`${color1}Taken`] = true
-      color = color1
-      IDS.get(socket.id).color = color1
+      if (newColor !== 'spectator' && ROOMS.get(room)[`${newColor}Taken`])
+         return socket.emit('error', 'selectColor: color already taken.')
+      
+      if (color !== 'spectator')
+         ROOMS.get(room)[`${color}Taken`] = false
+      ROOMS.get(room)[`${newColor}Taken`] = true
+
+      color = newColor
+      IDS.get(socket.id).color = newColor
       io.to(room).emit('player~', username, username, color, isOwner)
+
+      callback()
    })
 
    
    socket.on('coords', (coords) => {
+      if (!detailsOkCheck())
+         return
+      
       socket.to(room).emit('coords', color, coords)
       IDS.get(socket.id).x = coords.x
       IDS.get(socket.id).y = coords.y
