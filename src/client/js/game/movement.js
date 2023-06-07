6cf02ec7d6aadddf918d027bb20a2a2b8b21adcf
ezluci <ezluci1@gmail.com>
2023-06-07 14:51:27 +0300

    Now you can't move through bombs. Not sure how im gonna add the kickbombs powerup 💀

--- src/client/js/game/movement.js
+++ src/client/js/game/movement.js
@@ -2,7 +2,7 @@
 
 // returns true if you CANNOT GO through this block
 function stop(blockCode) {
-   return (blockCode !== BLOCK.NO && blockCode !== BLOCK.FIRE && blockCode !== BLOCK.BOMB && !isPowerup(blockCode))
+   return (blockCode !== BLOCK.NO && blockCode !== BLOCK.FIRE && !isPowerup(blockCode))
 }
 
 
@@ -10,19 +10,26 @@ function moveLeft() {
    if (me.x === MIN_X)
       return
 
-   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && me.x !== MIN_X && stop(map[me.y / BLOCK_SIZE][me.x / BLOCK_SIZE - 1]))
+   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && stop(map[me.y / BLOCK_SIZE][me.x / BLOCK_SIZE - 1]))
       return
 
    const mod = me.y % (2 * BLOCK_SIZE)
+
+   if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
+      return; // next to the player is a block. we don't move anything.
+   
+   const A = {x: Math.floor(me.x / BLOCK_SIZE), y: Math.floor(me.y / BLOCK_SIZE)};
+   const B = {x: Math.ceil(me.x / BLOCK_SIZE), y: Math.ceil(me.y / BLOCK_SIZE)};
+   if (A.wasBomb = (map[A.y][A.x] === BLOCK.BOMB))
+      map[A.y][A.x] = BLOCK.NO;
+   if (B.wasBomb = (map[B.y][B.x] === BLOCK.BOMB))
+      map[B.y][B.x] = BLOCK.NO;
 
    if (mod === 0) {
       me.x -= moveSpeed * deltaTime
       if (me.x < MIN_X || stop(map[me.y / BLOCK_SIZE][Math.floor(me.x / BLOCK_SIZE)]))
          me.x = Math.floor(meOld.x / BLOCK_SIZE) * BLOCK_SIZE
-   }
-   else if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
-      return // next to the player is a block. we don't move anything.
-   else {
+   } else {
       if (mod < BLOCK_SIZE && !stop(map[Math.floor(me.y / BLOCK_SIZE)][me.x / BLOCK_SIZE - 1])) {
          me.y -= moveSpeed * deltaTime
          const newMod = me.y % (2 * BLOCK_SIZE)
@@ -40,25 +47,38 @@ function moveLeft() {
          }
       }
    }
+
+   if (A.wasBomb)
+      map[A.y][A.x] = BLOCK.BOMB;
+   if (B.wasBomb)
+      map[B.y][B.x] = BLOCK.BOMB;
 }
+
 
 function moveDown() {
    if (me.y === MAX_Y)
       return
    
-   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && me.y !== MAX_Y && stop(map[me.y / BLOCK_SIZE + 1][me.x / BLOCK_SIZE]))
+   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && stop(map[me.y / BLOCK_SIZE + 1][me.x / BLOCK_SIZE]))
       return
    
    const mod = me.x % (2 * BLOCK_SIZE)
 
+   if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
+      return; // next to the player is a block. we don't move anything.
+
+   const A = {x: Math.floor(me.x / BLOCK_SIZE), y: Math.floor(me.y / BLOCK_SIZE)};
+   const B = {x: Math.ceil(me.x / BLOCK_SIZE), y: Math.ceil(me.y / BLOCK_SIZE)};
+   if (A.wasBomb = (map[A.y][A.x] === BLOCK.BOMB))
+      map[A.y][A.x] = BLOCK.NO;
+   if (B.wasBomb = (map[B.y][B.x] === BLOCK.BOMB))
+      map[B.y][B.x] = BLOCK.NO;
+   
    if (mod === 0) {
       me.y += moveSpeed * deltaTime
       if (me.y > MAX_Y || stop(map[Math.floor(me.y / BLOCK_SIZE) + 1][me.x / BLOCK_SIZE]))
          me.y = Math.floor(meOld.y / BLOCK_SIZE + 1) * BLOCK_SIZE
-   }
-   else if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
-      return
-   else {
+   } else {
       if (mod < BLOCK_SIZE && !stop(map[me.y / BLOCK_SIZE + 1][Math.floor(me.x / BLOCK_SIZE)])) {
          me.x -= moveSpeed * deltaTime
          const newMod = me.x % (2 * BLOCK_SIZE)
@@ -76,25 +96,38 @@ function moveDown() {
          }
       }
    }
+
+   if (A.wasBomb)
+      map[A.y][A.x] = BLOCK.BOMB;
+   if (B.wasBomb)
+      map[B.y][B.x] = BLOCK.BOMB;
 }
+
 
 function moveRight() {
    if (me.x === MAX_X)
       return
    
-   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && me.x !== MAX_X && stop(map[me.y / BLOCK_SIZE][me.x / BLOCK_SIZE + 1]))
+   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && stop(map[me.y / BLOCK_SIZE][me.x / BLOCK_SIZE + 1]))
       return
    
    const mod = me.y % (2 * BLOCK_SIZE)
 
+   if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
+      return; // next to the player is a block. we don't move anything.
+
+   const A = {x: Math.floor(me.x / BLOCK_SIZE), y: Math.floor(me.y / BLOCK_SIZE)};
+   const B = {x: Math.ceil(me.x / BLOCK_SIZE), y: Math.ceil(me.y / BLOCK_SIZE)};
+   if (A.wasBomb = (map[A.y][A.x] === BLOCK.BOMB))
+      map[A.y][A.x] = BLOCK.NO;
+   if (B.wasBomb = (map[B.y][B.x] === BLOCK.BOMB))
+      map[B.y][B.x] = BLOCK.NO;
+   
    if (mod === 0) {
       me.x += moveSpeed * deltaTime
       if (me.x > MAX_X || stop(map[me.y / BLOCK_SIZE][Math.floor(me.x / BLOCK_SIZE) + 1]))
          me.x = Math.floor(meOld.x / BLOCK_SIZE + 1) * BLOCK_SIZE
-   }
-   else if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
-      return
-   else {
+   } else {
       if (mod < BLOCK_SIZE && !stop(map[Math.floor(me.y / BLOCK_SIZE)][me.x / BLOCK_SIZE + 1])) {
          me.y -= moveSpeed * deltaTime
          const newMod = me.y % (2 * BLOCK_SIZE)
@@ -112,25 +145,38 @@ function moveRight() {
          }
       }
    }
+
+   if (A.wasBomb)
+      map[A.y][A.x] = BLOCK.BOMB;
+   if (B.wasBomb)
+      map[B.y][B.x] = BLOCK.BOMB;
 }
+
 
 function moveUp() {
    if (me.y === MIN_Y)
       return
    
-   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && me.y !== MIN_Y && stop(map[me.y / BLOCK_SIZE - 1][me.x / BLOCK_SIZE]))
+   if (me.x % BLOCK_SIZE === 0 && me.y % BLOCK_SIZE === 0 && stop(map[me.y / BLOCK_SIZE - 1][me.x / BLOCK_SIZE]))
       return
    
    const mod = me.x % (2 * BLOCK_SIZE)
 
+   if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
+      return; // next to the player is a block. we don't move anything.
+
+   const A = {x: Math.floor(me.x / BLOCK_SIZE), y: Math.floor(me.y / BLOCK_SIZE)};
+   const B = {x: Math.ceil(me.x / BLOCK_SIZE), y: Math.ceil(me.y / BLOCK_SIZE)};
+   if (A.wasBomb = (map[A.y][A.x] === BLOCK.BOMB))
+      map[A.y][A.x] = BLOCK.NO;
+   if (B.wasBomb = (map[B.y][B.x] === BLOCK.BOMB))
+      map[B.y][B.x] = BLOCK.NO;
+   
    if (mod === 0) {
       me.y -= moveSpeed * deltaTime
       if (me.y < MIN_Y || stop(map[Math.floor(me.y / BLOCK_SIZE)][me.x / BLOCK_SIZE]))
          me.y = Math.floor(meOld.y / BLOCK_SIZE) * BLOCK_SIZE
-   }
-   else if (BLOCK_SIZE - BLOCK_SAFE_PX <= mod && mod <= BLOCK_SIZE + BLOCK_SAFE_PX)
-      return
-   else {
+   } else {
       if (mod < BLOCK_SIZE && !stop(map[me.y / BLOCK_SIZE - 1][Math.floor(me.x / BLOCK_SIZE)])) {
          me.x -= moveSpeed * deltaTime
          const newMod = me.x % (2 * BLOCK_SIZE)
@@ -148,4 +194,9 @@ function moveUp() {
          }
       }
    }
+
+   if (A.wasBomb)
+      map[A.y][A.x] = BLOCK.BOMB;
+   if (B.wasBomb)
+      map[B.y][B.x] = BLOCK.BOMB;
 }
