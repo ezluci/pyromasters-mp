b67b3896785ecd366ac058a4d02f893b6c80047c
ezluci <ezluci1@gmail.com>
2024-05-24 14:20:23 +0300

    Fixed name checking on room entering.

--- src/server/events/playerJoined.js
+++ src/server/events/playerJoined.js
@@ -4,12 +4,12 @@ const CONST = require('../consts')()
 
 function playerJoined(username, room, callback, io, ROOMS, sok) {
    
-   if (! /^[a-z0-9]+$/i.test(username)) {
+   if (! /^[a-zA-Z0-9]+$/i.test(username)) {
       sok.emit('error', 'playerJoined: invalid username. DISCONNECTED.');
       sok.disconnect();
       return;
    }
-   if (! /^[a-z0-9]+$/i.test(room)) {
+   if (! /^[a-zA-Z0-9]+$/i.test(room)) {
       sok.emit('error', 'playerJoined: invalid room. DISCONNECTED.');
       sok.disconnect();
       return;
